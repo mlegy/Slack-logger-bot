2af0ab745f99c1a8fc4de6c659b00dee4e98487d
JYNY <simon54894@gmail.com>
2015-08-22 14:06:29 +0800

    add seconds timestamp

--- logbot.js
+++ logbot.js
@@ -20,7 +20,7 @@ module.exports = function(req, res, next) {
     // auto respond to some messages --> implement it yourself
     //respond(); --> NOT HERE (CAT bot ONLY)
 
-    function getDate(timestamp) {
+    function getDate(timestamp) 
         // create a new javascript Date object based on the timestamp
         // multiplied by 1000 so that the argument is in milliseconds, not seconds
         var date = new Date(timestamp * 1000);
@@ -29,9 +29,9 @@ module.exports = function(req, res, next) {
         // minutes part from the timestamp
         var minutes = "0" + date.getMinutes();
         // minutes part from the timestamp
-        var seconds = "0" + date.getSeconds();
+        var seconds = date.getSeconds();
         // will display time in 10:30 format
-        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + minutes.substr(-2);
+        var formattedTime = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
         return formattedTime;
     }
 
